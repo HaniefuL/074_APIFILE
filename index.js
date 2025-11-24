86262875607f025af1e2b6a4a1f0a7d6892334eb
HaniefuL <haniefulgeofarry@gmail.com>
2025-11-24 11:14:03 +0700

    SELESAI

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const connectDataBase = require('./config/db');
-
+const apiRoute = require('./routes/api');
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use('/api/v1', apiRoute);
 
 async function startServer() {
     await connectDataBase();
--- models/komik.js
+++ models/komik.js
@@ -1,28 +1,26 @@
 module.exports = (sequelize, DataTypes) => {
-const Komik = sequelize.define('Komik', {
-    id: {
-        type: DataTypes.INTEGER,
-        autiIncrement: true,
-        primaryKey: true
+    const Komik = sequelize.define("Komik", {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        title: {
+            type: DataTypes.STRING,
+        },
+        description: {
+            type: DataTypes.STRING,
+        },
+        author: {
+            type: DataTypes.STRING,
+        },
+        imageType: DataTypes.STRING,
+        imageType: DataTypes.STRING,
+        imageData: DataTypes.BLOB('long'),
     },
-    title: {
-        type: DataTypes.STRING,
-
-    },
-    description: {
-        type: DataTypes.STRING,
-    },
-    author: {
-        type: DataTypes.STRING,
-    },
-    imageType: DataTypes.STRING,
-    imageName: DataTypes.STRING,
-    ImageData: DataTypes.BLOB('long')
-
-    }, 
     {
-        tableNamame: 'komik',
+        tableName: 'komiks',
     }
 );
     return Komik;
-}
+};
